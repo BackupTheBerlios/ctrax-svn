42ba7b6f7385fe11de1132161a86a0b6a12ca049
jbender <jbender@f5491480-045c-0410-8d49-a9476301468f>
2011-04-21 04:58:22 +0000

    branch for Mac build

---
+++ branches/mac-build/Ctrax_mac/__init__.py
@@ -0,0 +1,8 @@
+"""Ctrax
+
+The Caltech Multiple Fly Tracker.
+
+Caltech ethomics project, 2007-2011
+"""
+
+from Ctrax_mac import main, CtraxApp, __version__
---
+++ branches/mac-build/matlab/filehandling/load_tracks.m
@@ -0,0 +1,65 @@
+% [trx,matname,succeeded] = load_tracks(matname,[moviename])
+
+function [trx,matname,succeeded] = load_tracks(matname,moviename,varargin)
+
+[dosave,savename,annname] = myparse(varargin,'dosave',false,'savename','','annname','');
+
+succeeded = false;
+trx = [];
+
+isinteractive = ~exist('matname','var');
+
+if isinteractive,
+  helpmsg = 'Choose mat file containing trajectories to load';
+  [matname,matpath] = uigetfilehelp('*.mat','Choose mat file containing trajectories','','helpmsg',helpmsg);
+  if ~ischar(matname),
+    return;
+  end
+  matname = [matpath,matname];
+end
+
+tmp = load(matname);
+fprintf('loaded %s\n',matname);
+if isfield(tmp,'pairtrx'),
+  tmp.trx = tmp.pairtrx;
+end
+if ~isfield(tmp,'trx'),
+  fprintf('no trx variable\n');
+  if isfield(tmp,'ntargets'),
+    fprintf('Ctrax output file; converting to trx file\n');
+    if ~exist('moviename','var'),
+      moviename = '?';
+    end
+    %ds = datestr(now,30);
+    fprintf('Calling cleanup_ctrax_data\n');
+    [trx,matname,timestamps] = cleanup_ctrax_data(matname,moviename,tmp,'','dosave',dosave,'savename',savename,'annname',annname);
+  else
+    msgbox('Could not load data from %s, exiting',matname);
+    return;
+  end
+else
+  fprintf('trx variable found\n');
+  trx = tmp.trx;
+  if exist('moviename','var') && ~isfield(trx,'moviename'),
+    for i = 1:length(trx),
+      trx(i).moviename = moviename;
+    end
+  end
+  if dosave && ~isempty(savename),
+      [didcopy,msg,~] = copyfile(matname,savename);
+      if ~didcopy,
+          error('Could not copy %s to %s:\n%s',matname,savename,msg);
+      end
+  end
+      
+end
+
+% member functions can be weird
+fprintf('Adding off\n');
+for i = 1:length(trx),
+  trx(i).off = -trx(i).firstframe + 1;
+  trx(i).matname = matname;
+end
+
+succeeded = true;
+fprintf('Done. returning from load_tracks\n');
