7e3b8d7bc0b64211377ae521be1504670ea04db7
jbender <jbender@f5491480-045c-0410-8d49-a9476301468f>
2011-04-21 05:09:22 +0000

    rename Ctrax

--- branches/mac-build/Ctrax/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Ctrax
-
-The Caltech Multiple Fly Tracker.
-
-Caltech ethomics project, 2007-2008
-"""
-
-from Ctrax import main, CtraxApp, __version__
--- branches/mac-build/Ctrax/codedir.py
+++
@@ -1,5 +0,0 @@
-import os
-codedir = os.path.dirname(__file__)
-(head,tail) = os.path.split(codedir)
-if tail == 'library.zip':
-    codedir = head
